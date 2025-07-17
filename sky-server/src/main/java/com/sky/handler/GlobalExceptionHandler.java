9d60d35f05540dd30abdd6be2c3a553568741718
Tr3nchance <120464959+trenchance2022@users.noreply.github.com>
2025-07-17 15:53:27 +0800

    新增员工

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,22 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 处理SQL异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String msg=ex.getMessage();
+        log.error("异常信息：{}", ex.getMessage());
+        if(msg.contains("Duplicate entry")){
+            String[] tmp=msg.split(" ");
+            String username=tmp[2];
+            String errMsg=username+" 已存在";
+            return Result.error(errMsg);
+        } else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
