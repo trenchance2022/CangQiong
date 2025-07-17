52c65c9a65dbc113a3435fd2cd9bb70791b0e9c9
Tr3nchance <120464959+trenchance2022@users.noreply.github.com>
2025-07-17 21:21:53 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -34,8 +34,16 @@ public interface EmployeeMapper {
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
-     *
+     * 更新员工信息
      * @param employee
      */
     void update(Employee employee);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
