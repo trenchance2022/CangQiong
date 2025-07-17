d142aa36e88fcf7046dacab0887a8f0548cf6855
Tr3nchance <120464959+trenchance2022@users.noreply.github.com>
2025-07-17 11:23:24 +0800

    修改登录

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -9,11 +9,9 @@ import java.io.Serializable;
 @Data
 @ApiModel(description = "员工登录时传递的数据模型")
 public class EmployeeLoginDTO implements Serializable {
-
     @ApiModelProperty("用户名")
     private String username;
 
     @ApiModelProperty("密码")
     private String password;
-
 }
